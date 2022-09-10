b14bbeac4013fd9a616c67f30595bb879fb82677
Vladislav Kireev <kireew.vladislaw@gmail.com>
2022-09-10 23:08:44 +0300

    feature: blocks header, introduction, description, training

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,1 +1,23 @@
 import './index.scss';
+
+
+
+import introductionImage from '../images/introduction-image.jpg';
+import introductionBike from '../images/bike-fix.png';
+import descriptionImage from '../images/description-image.jpg';
+import trainingImage from '../images/training-image.jpg';
+
+const introductionSection = document.querySelector('.introduction');
+
+const introductionImageBlock = introductionSection.querySelector('.introduction__image');
+const introductionBikeBlock = introductionSection.querySelector('.introduction__bike-image');
+introductionImageBlock.src = introductionImage;
+introductionBikeBlock.src = introductionBike;
+
+const descriptionSection = document.querySelector('.description');
+const descriptionImageBlock = descriptionSection.querySelector('.description__image');
+descriptionImageBlock.src = descriptionImage;
+
+const trainingSection = document.querySelector('.training');
+const trainingImageBlock = trainingSection.querySelector('.training__image');
+trainingImageBlock.src = trainingImage;
